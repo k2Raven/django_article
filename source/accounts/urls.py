3d3eae5b3474d6875b586e84fbf741e63101ef14
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2024-01-09 21:13:59 +0600

    результат занятия 63

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserDetailView, UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -8,5 +8,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='user_create')
+    path('register/', RegisterView.as_view(), name='user_create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('<int:pk>/change/', UserChangeView.as_view(), name='user_change'),
+    path('<int:pk>/password_change/', UserPasswordChangeView.as_view(), name='password_change')
 ]
--- source/hello/settings.py
+++ source/hello/settings.py
@@ -128,3 +128,6 @@ LOGIN_URL = 'accounts:login'
 
 LOGIN_REDIRECT_URL = 'webapp:index'
 LOGOUT_REDIRECT_URL = 'accounts:login'
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'uploads')
+MEDIA_URL = '/media/'
