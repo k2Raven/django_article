85403e045737fc72589a9bd9a96233267788535d
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2024-01-31 18:34:25 +0600

    результат занятия 69.

--- source/hello/urls.py
+++ source/hello/urls.py
@@ -19,8 +19,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+urls_app = [
+    path('v1/', include('api_v1.urls'))
+]
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('api/', include(urls_app))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
